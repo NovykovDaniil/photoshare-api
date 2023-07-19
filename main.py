a9e0a41b22ea1b9596926fb35d79f03248b1182b
NovykovDaniil2 <relax2934@gmail.com>
2023-07-19 20:00:52 +0300

    Init

---
+++ main.py
@@ -0,0 +1,30 @@
+import redis.asyncio as redis
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.routes import auth, tags, photos, users, comments, estimates
+
+
+app = FastAPI()
+
+
+app.include_router(auth.router, prefix="/api")
+app.include_router(tags.router, prefix="/api")
+app.include_router(photos.router, prefix="/api")
+app.include_router(users.router, prefix="/api")
+app.include_router(comments.router, prefix="/api")
+app.include_router(estimates.router, prefix="/api")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hi! Welcome to the photo share!"}
---
+++ src/conf/config.py
@@ -0,0 +1,25 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    sqlalchemy_database_url: str
+    secret_key: str
+    algorithm: str
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_port: int
+    mail_server: str
+    redis_host: str = 'localhost'
+    redis_port: int = 6379
+    redis_password: str = 'password'
+    cloudinary_name: str
+    cloudinary_api_key: str
+    cloudinary_api_secret: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
