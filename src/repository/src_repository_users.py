ef16b9b07b1a9c4114cc6ae0bf7618e214c29301
Serhii Budnikov <62923860+serjbuda@users.noreply.github.com>
2023-07-20 19:51:41 +0300

    add src_repository_users

---
+++ src/repository/src_repository_users.py
@@ -0,0 +1,80 @@
+from libgravatar import Gravatar
+from sqlalchemy.orm import Session
+
+from fastapi import HTTPException, status
+
+from src.database.models import User, UserRole
+from src.schemas import UserModel
+from src.messages import *
+
+
+async def get_user_by_email(email: str, db: Session) -> User:
+    return db.query(User).filter(User.email == email).first()
+
+
+async def create_user(body: UserModel, db: Session) -> User:
+    avatar = None
+    try:
+        gr = Gravatar(body.email)
+        avatar = gr.get_image()
+    except Exception as err:
+        print(err)
+    new_user = User(**dict(body), avatar=avatar)
+    is_first_user = db.query(User).first()
+    if is_first_user is None:
+        new_user.role = UserRole.ADMIN
+        new_user.confirmed = True
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+async def update_token(user: User, token: str | None, db: Session) -> None:
+    user.refresh_token = token
+    db.commit()
+
+
+async def confirmed_email(email: str, db: Session) -> None:
+    user = await get_user_by_email(email, db)
+    user.confirmed = True
+    db.commit()
+
+
+async def update_avatar(email, url: str, db: Session) -> User:
+    user = await get_user_by_email(email, db)
+    user.avatar = url
+    db.commit()
+    return user
+
+
+async def change_password(user: User, new_password: str, db: Session) -> None:
+    user.password = new_password
+    db.commit()
+
+
+async def get_user(username: str, db: Session) -> User:
+    user = db.query(User).filter(User.username == username).first()
+    return user
+
+
+async def edit_user(username: str, bio: str, user: User, db: Session) -> User:
+    user_ = await get_user(username, db)
+    if user_.username != user.username:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=NOT_YOUR_ACCOUNT
+        )
+    user_.bio = bio
+    db.add(user_)
+    db.commit()
+    db.refresh(user_)
+    return user_
+
+
+async def change_user_active(username: str, db: Session):
+    user_ = await get_user(username, db)
+    user_.is_active = not user_.is_active
+    db.add(user_)
+    db.commit()
+    db.refresh(user_)
+    return user_
